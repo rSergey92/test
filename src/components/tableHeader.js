b375df25eba531d5289c2a26036e3f6125f10fac
rSergey92 <mr.romash1992@mail.ru>
2020-07-27 22:37:00 +0300

    далил неиспользуемый код

--- src/components/tableHeader.js
+++ src/components/tableHeader.js
@@ -17,13 +17,6 @@ export default class TableHeader {
         const trow = document.createElement('tr');
         const textHeader = ['id', 'firstName', 'lastName', 'email', 'phone'];
 
-        const comparator = (index, order) => {
-            console.log(index, order);
-            return function sort(a, b) {
-                return a - b;
-            }
-        }
-
         for(let i = 0; i < textHeader.length; i++) {
             let th = document.createElement('th');
             let icon = document.createElement('i');
@@ -36,25 +29,7 @@ export default class TableHeader {
             this.addStyle(th);
             th.addEventListener('click', () => {
                 this.changeClass(icon);
-                window.EventBus.on(EVENT.SORT_CELL, {
-                    field: th.getAttribute('data-field'),
-                });
-                window.TableBody.startSort();
             });
-            // Решение 
-            // https://inter-net.pro/javascript/sort-table
-            //https://www.insales.ru/collection/doc-funktsional-js/product/funktsional-js-shina-sobytiy
-
-            th.addEventListener('click', ({ target }) => {
-            const order = (target.dataset.order = -(target.dataset.order || -1));
-            const index = [...target.parentNode.cells].indexOf(target);
-
-            for(const tBody of target.closest('table').tBodies) {
-                tBody.append(...[...tBody.rows].sort(comparator(index, order)));
-                console.log(tBody);
-            }
-        });
-
 
             trow.appendChild(th);
         }
