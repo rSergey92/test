7185bc543768d341440ef537ed2c908098cd8824
rSergey92 <mr.romash1992@mail.ru>
2020-07-27 22:34:25 +0300

    Получение данных

--- src/api/index.js
+++ src/api/index.js
@@ -1,20 +1,23 @@
-const makeReauest = function(path, options = {}) {
-     fetch(`path`, options)
-}
-
-export default class Api {
+class Api {
     constructor() {
         this.domain = '';
     }
 
+    async makeRequest({ path }) {
+        const response = await fetch(path);
+        if (response.status === 200) return response.json();
+    }
+
     getSmallData() {
-        return makeRequest({
-            url: 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}',
+        return this.makeRequest({
+            path: 'http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}',
         })
     }
     getBigData() {
-        return makeRequest({
-            url: 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}',
+        return this.makeRequest({
+            path: 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&adress={addressObject}&description={lorem|32}',
         })
     }
 }
+
+export default new Api();
